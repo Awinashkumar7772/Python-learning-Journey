db00478389724c88f06f7645192cbdb280e07ac4
Awinash kumar <kawinash10@gmail.com>
2026-06-01 22:17:58 +0530

    Day1 of python completed

---
+++ Day1/Datatypes.py
@@ -0,0 +1,3 @@
+#Dataypes are the container where we assign value acording to the there types
+#as Integer store only integer data,String store character and letters and same for all the datatypes.
+#All the datatypes are given below 1.Interger,2,Float,3.Boolean,4.String,5.List,6.Tuple and 7.Dictionary.
---
+++ Day1/Variables.py
@@ -0,0 +1,2 @@
+#Variables are the containers which are used to store the data of all datatypes in the container.
+#like x = 10; Here we can see 10 is being assigned to be the x .
