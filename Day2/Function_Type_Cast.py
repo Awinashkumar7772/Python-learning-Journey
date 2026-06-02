1659d15824bbe7812e17979ec726a6dffd058bf3
Awinash kumar <kawinash10@gmail.com>
2026-06-02 21:07:36 +0530

    Add files via upload

---
+++ Day2/Function_Type_Cast.py
@@ -0,0 +1,7 @@
+#Type() with this function we can check the type of variables
+a = 10
+print(type(a))
+
+b = 11
+str(11)
+print(type(b))
---
+++ Day2/input.py
@@ -0,0 +1,6 @@
+a = int(input("enter the value of a"))
+b = int(input('Enter the value of b'))
+
+print("Sum of a and b is : ", a+b)
+
+#Here input are always to be consider as String because of the input taken from command line.
